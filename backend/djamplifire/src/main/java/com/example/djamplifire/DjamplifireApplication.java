25e8238dd8a8898660c5af3f2621b87ecb86cc14
TCON163 <tylerconnercareersearch@gmail.com>
2021-10-29 23:14:20 -0500

    set up spring

---
+++ backend/djamplifire/src/main/java/com/example/djamplifire/DjamplifireApplication.java
@@ -0,0 +1,13 @@
+package com.example.djamplifire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjamplifireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjamplifireApplication.class, args);
+	}
+
+}
---
+++ backend/djamplifire/src/test/java/com/example/djamplifire/DjamplifireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.djamplifire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjamplifireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
