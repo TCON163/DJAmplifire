2fbdc2525f6eb63ad7d00b902c42c987abf3c720
Emmanuel <emmanuel_tejeda@my.uri.edu>
2021-11-17 16:04:45 -0500

    unit test 3

--- frontend/djamplifire-frontend/src/app/create-room/create-room.component.spec.ts
+++ frontend/djamplifire-frontend/src/app/create-room/create-room.component.spec.ts
@@ -1,25 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RoomService } from './../room.service';
+import {
+  ComponentFixture,
+  TestBed,
+  tick,
+  fakeAsync,
+} from '@angular/core/testing';
+import { ActivatedRoute, Params } from '@angular/router';
 import { CreateRoomComponent } from './create-room.component';
 
 describe('CreateRoomComponent', () => {
   let component: CreateRoomComponent;
   let fixture: ComponentFixture<CreateRoomComponent>;
+  let testService: ActivatedRoute;
+  let roomTestService: RoomService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateRoomComponent ]
-    })
-    .compileComponents();
+      declarations: [CreateRoomComponent],
+      providers: [RoomService],
+    }).compileComponents();
   });
 
+  //basic boiler plate testing for compnents.
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateRoomComponent);
     component = fixture.componentInstance;
+
+    //checks to see if there are any changes in the component
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //checks to see if the subscribe button worked
+  it('testing subscribe method is getting called', fakeAsync(() => {
+    let subSpy = spyOn(testService.queryParamMap, 'subscribe');
+    component.ngOnInit();
+
+    tick();
+    expect(subSpy).toHaveBeenCalled();
+  }));
 });
